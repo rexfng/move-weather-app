167c4c54e2b8b86fb955ee65914f87ceaa8c5d0c
Rex Fong <rexfng@gmail.com>
2017-03-22 21:42:15 -0700

    create class weather and get_weather function

--- weather.php
+++ weather.php
@@ -2,10 +2,15 @@
 	/**
 	* Retreive from Yahoo Weather api for weather information for cities
 	*/
-	class Weather{
-		function __construct(argument)
-		{
-			# code...
+	class weather{
+		function get_weather($place, $region){
+			$url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22" . $place . "," . $region . "%E2%80%8B%22)&format=json";
+			$curl = 		 curl_init($url);
+							 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+			$curl_response = curl_exec($curl);
+							 curl_close($curl);
+			$decoded = json_decode($curl_response, true);
+			return $decoded;
 		}
 	}
 ?>
